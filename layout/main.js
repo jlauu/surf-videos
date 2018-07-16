4040f91dbc248e157bba1deae3da6f11607a0dab
Johnny Lau <johnny.df.lau@gmail.com>
2018-07-15 23:28:35 -0400

    Just use a cdn -_-

--- layout/main.js
+++ layout/main.js
@@ -1,17 +1,10 @@
-import 'semantic-ui-css/components/container.css';
-import 'semantic-ui-css/components/embed.css';
-import 'semantic-ui-css/components/header.css';
-import 'semantic-ui-css/components/image.css';
-import 'semantic-ui-css/components/list.css';
-import 'semantic-ui-css/themes/default/assets/fonts/icons.eot';
-import 'semantic-ui-css/themes/default/assets/fonts/icons.woff';
-import 'semantic-ui-css/themes/default/assets/fonts/icons.woff2';
 import Head from 'next/head';
 import { Container } from 'semantic-ui-react';
 
 export default ({ children }) => (
   <div>
     <Head>
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css"></link>
       <link rel="stylesheet" href="/_next/static/style.css" />
     </Head>
     <Container>
